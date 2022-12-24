ea806f8536e79f890c122c23e8f4deef7e3095c9
thabet <mahmous010266@gmail.com>
2022-12-24 08:44:51 +0200

    proc and fork

--- process.c
+++ process.c
@@ -2,19 +2,42 @@
 
 /* Modify this file as needed*/
 int remainingtime;
+//--------------------------
+void handler(int signum)
+{
+  printf("\n\n TimeOut......\n");
+  exit(9);
+}
+//---------------------------
+
 
 int main(int agrc, char * argv[])
 {
+    signal(SIGALRM, handler);
     initClk();
-    
+    printf("clk inside process %d\n",getClk());
     //TODO it needs to get the remaining time from somewhere
     //remainingtime = ??;
+    remainingtime=atoi(argv[1]);
+
     while (remainingtime > 0)
     {
         // remainingtime = ??;
+        //sleep(1);
+        alarm(remainingtime);
+        for (int i=0;i<remainingtime;i++){
+            sleep(1);
+            printf("time in p1 is %d\n",getClk());
+        }
+
+
+        //remainingtime--;
+
     }
     
     destroyClk(false);
     
     return 0;
 }
+
+
--- scheduler.c
+++ scheduler.c
@@ -1,6 +1,43 @@
 #include "headers.h"
+#include <stdio.h>
+#include <stdlib.h>
+ #include <string.h>
+//-------------------------------------------------------
+ /* reverse:  reverse string s in place */
+ void reverse(char s[])
+ {
+     int i, j;
+     char c;
+
+     for (i = 0, j = strlen(s)-1; i<j; i++, j--) {
+         c = s[i];
+         s[i] = s[j];
+         s[j] = c;
+     }
+}  
+
+ char* itoa(int n, char s[])
+ {
+     int i, sign;
+
+     if ((sign = n) < 0)  /* record sign */
+         n = -n;          /* make n positive */
+     i = 0;
+     do {       /* generate digits in reverse order */
+         s[i++] = n % 10 + '0';   /* get next digit */
+     } while ((n /= 10) > 0);     /* delete it */
+     if (sign < 0)
+         s[i++] = '-';
+     s[i] = '\0';
+     reverse(s);
+     return s;
+     
+}  
 
 
+//---------------------------------------------------------------
+
+char snum[10];   //to use while converting from int to char
 
 typedef struct msgbuff  // buffer to store process in
 {
@@ -41,36 +78,86 @@ typedef struct msgbuff  // buffer to store process in
 //     fclose(file);
 // }
 
+/// @brief function to equate two the pcb to message recieved
+/// @param p process
+/// @param message message
+process eq_st(process p,msgbuff message){
+    p.ArrTime=message.ArrTime;
+    p.ID=message.ID;
+    p.Priority=message.Priority;
+    p.RunTime=message.RunTime;
+    return p;
+}
+
+
 int main(int argc, char * argv[])
 {
-    //initClk();
+
+    //----------------  some inititalizations  --------------------
+    initClk();
+    printf("%d\n",getClk());
     printf("im a scheduler\n");
     printf("the algotihm type is %s\n",argv[1]);
-
-
+    //-----------------------------
     int downQ;
     key_t key_down =123;
     downQ=msgget(key_down, IPC_CREAT | 0644);
-
     msgbuff message; 
+    process p;
+//---------------------------------
+    heap_t *readyq_sjf = (heap_t *)calloc(1, sizeof(heap_t));
+//----------------------------------
+void fork_process(char* rem_time){
+
+    int pid;
+    pid= fork();
+    if (pid ==0)
+    {
+        printf("process has been forked \n");
+        char *args[]= {"./process.out",rem_time,NULL};
+        execv(args[0],args);
+        printf("didn'twork");
+    }
+    // else{
+    //     wait()
+    // }
+}
+
     while (1)
     {
     int rec_val = msgrcv(downQ, &message, sizeof(message),0, !IPC_NOWAIT);
-    if(rec_val==-1){
-        perror("msg not recieved");
-    }
-        printf("message is : %d\n", message.Priority);
-        // process *p;
-        // p->ID=message.ID;
-        // p->ArrTime=message.ArrTime;
-        // p->Priority=message.Priority;
-        // p->RunTime=message.RunTime;
-        // report_file(*p);
+
+        if(rec_val==-1){
+            perror("msg not recieved");
+        }
+        else{
+        //push(readyq_sjf,p.ArrTime,&p[i]);
+            printf("message recieved\n");
+            printf("clk is %d\n ",getClk());
+            process p=eq_st(p,message);
+            printf("arr_time of p %d\n",p.ArrTime);
+            printf("message id : %d\n", message.RunTime);
+            push(readyq_sjf,p.RunTime,&p);
+            while(readyq_sjf->len){
+                process pf =*pop(readyq_sjf);
+                fork_process(itoa(pf.RunTime,snum));
+            }
+
+    
+            //process P_to_fork =*pop(readyq_sjf);
+
+            //printf("run time of p1 %d\n",P_to_fork.RunTime);
+        }
+            printf("run time of p %d\n",top(readyq_sjf)->RunTime);
+
+    
+ 
+
+
+
+
     }
 
 
-    //TODO implement the scheduler :)
-    //upon termination release the clock resources
-    // sleep(4);
-    //destroyClk(true);
+
 }
